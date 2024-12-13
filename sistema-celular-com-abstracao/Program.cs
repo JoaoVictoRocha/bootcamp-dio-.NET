849f40ef409c3c21a4a661eec0b292e918f709f4
João Rocha <joaovictor75756@gmail.com>
2024-12-12 21:03:57 -0300

    Adicionando a atividade referente a criação de um sistema e abstração com celular

---
+++ sistema-celular-com-abstracao/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using sistema_celular_com_abstracao.Models;
+
+Nokia celular1 = new Nokia("99 99999-9999", "Nokia 3200", "26841698", 128);
+Iphone celular2 = new Iphone("88 88888-8888", "Iphone 11", "138413487", 128);
+
+celular1.InstalarAplicativo("Facebook");
+celular2.InstalarAplicativo("Instagram");
+
